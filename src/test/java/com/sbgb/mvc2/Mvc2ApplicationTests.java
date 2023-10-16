26efed9ed0de9c2b877ca4357fbceaab1457ca9c
subeomi <tnqjarnqja@gmail.com>
2023-10-16 18:53:34 +0900

    Initial commit

---
+++ src/test/java/com/sbgb/mvc2/Mvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbgb.mvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
